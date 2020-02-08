a60d5d3e2fd3aac2b3200370dceaee568d18284f
giuscia <giuseppe.sciacca@gmail.com>
2020-02-08 09:57:26 +0100

    Update wp-extended-data-to-rest-api.php

--- wp-extended-data-to-rest-api.php
+++ wp-extended-data-to-rest-api.php
@@ -13,9 +13,10 @@
  * Plugin URI: https://github.com/giuscia/wp-extended-data-to-rest-api
  * Text Domain: wp-extended-data-to-rest-api
  */
-if ( !defined( 'ABSPATH' ) ) {
+
+if ( !defined( 'ABSPATH' ) ):
 	exit;
-}
+endif;
 
 
 
