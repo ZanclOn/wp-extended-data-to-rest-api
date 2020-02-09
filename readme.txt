f775d99bc52d9b671e418d9f504750f52d82844c
Giuseppe Sciacca <g.sciacca@glauco.it>
2020-02-09 15:31:51 +0100

    tuning

--- readme.txt
+++ readme.txt
@@ -3,9 +3,9 @@
  * Version: 1.0.0
  * Requires at least: 5.2
  * Requires PHP:      5.6
- * Author: giuscia
- * Author URI: https://github.com/giuscia/
+ * Author: ZanclOn
+ * Author URI: https://github.com/ZanclOn/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Plugin URI: https://github.com/giuscia/wp-extended-data-to-rest-api
+ * Plugin URI: https://github.com/ZanclOn/wp-extended-data-to-rest-api
  * Text Domain: wp-extended-data-to-rest-api
--- wp-extended-data-to-rest-api.php
+++ wp-extended-data-to-rest-api.php
@@ -3,14 +3,14 @@
 /**
  * Plugin Name: Extended Data to REST API
  * Description: Exposes Extended Data (meta, terms, p2p, ...) in the WordPress REST API
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 5.2
  * Requires PHP:      5.6
  * Author: giuscia
- * Author URI: https://github.com/giuscia/
+ * Author URI: https://github.com/ZanclOn/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Plugin URI: https://github.com/giuscia/wp-extended-data-to-rest-api
+ * Plugin URI: https://github.com/ZanclOn/wp-extended-data-to-rest-api
  * Text Domain: wp-extended-data-to-rest-api
  */
 
