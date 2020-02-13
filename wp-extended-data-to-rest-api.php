ca0419f3ff5e25aafe2afde5effe9f74fe6e0b12
giuscia <giuseppe.sciacca@gmail.com>
2020-02-13 21:57:31 +0100

    Update wp-extended-data-to-rest-api.php

--- wp-extended-data-to-rest-api.php
+++ wp-extended-data-to-rest-api.php
@@ -3,10 +3,10 @@
 /**
  * Plugin Name: Extended Data to REST API
  * Description: Exposes Extended Data (meta, terms, p2p, ...) in the WordPress REST API
- * Version: 1.0.1
+ * Version: 1.0.2
  * Requires at least: 5.2
  * Requires PHP:      5.6
- * Author: giuscia
+ * Author: ZanclOn
  * Author URI: https://github.com/ZanclOn/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
